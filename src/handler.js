b97223067222c8ca655cca6cd78214669a1156e0
Husni Zuhdi <hazunanafaru@gmail.com>
2022-03-13 20:38:23 +0700

    Add posting note, get notes, and get note by id

--- src/handler.js
+++ src/handler.js
@@ -35,4 +35,32 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = addNoteHandler;
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteHandler = (request, h) => {
+  const {id} = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Note is not found',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {addNoteHandler, getAllNotesHandler, getNoteHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
-const addNoteHandler = require('./handler');
+/* eslint-disable max-len */
+const {addNoteHandler, getAllNotesHandler, getNoteHandler} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteHandler,
   },
 ];
 
